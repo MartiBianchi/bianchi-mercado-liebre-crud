45fb009f239cf1c1a9286239d56edb4a644e0958
Martiniano <bianchimartiniano@gmail.com>
2023-10-27 13:28:30 -0300

    correecion de nombre de vista

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,19 +1,19 @@
-const productsServices = require("../services/productsServices");
+const productsServices = require('../services/productsServices')
 
 const controller = {
   index: (req, res) => {
-    const products = productsServices.getAllProducts();
+    const products = productsServices.getAllProducts()
 
-    res.render("products", { products });
+    res.render('products', { products })
   },
   detail: (req, res) => {
-    const id = req.params.id;
-    const product = productsServices.getProduct(id);
+    const id = req.params.id
+    const product = productsServices.getProduct(id)
 
-    res.render("detail", { product });
+    res.render('product-detail', { product })
   },
   create: (req, res) => {
-    res.render("product-create-form");
+    res.render('product-create-form')
   },
   store: (req, res) => {
     const product = {
@@ -22,38 +22,38 @@ const controller = {
       category: req.body.category,
       price: Number(req.body.price),
       discount: Number(req.body.discount),
-      image: req.file ? req.file.filename : "default-image.jpg",
-    };
+      image: req.file ? req.file.filename : 'default-image.jpg',
+    }
 
-    productsServices.createProduct(product);
+    productsServices.createProduct(product)
 
-    res.redirect("/products");
+    res.redirect('/products')
   },
   edit: (req, res) => {
-    const id = req.params.id;
-    const product = productsServices.getProduct(id);
+    const id = req.params.id
+    const product = productsServices.getProduct(id)
 
-    res.render("product-edit-form", { product });
+    res.render('product-edit-form', { product })
   },
   update: (req, res) => {
-    const product = req.body;
-    const id = req.params.id;
+    const product = req.body
+    const id = req.params.id
     const image = req.file
       ? req.file.filename
-      : productsServices.getProduct(id).image;
+      : productsServices.getProduct(id).image
 
-    product.image = image;
-    productsServices.updateProduct(id, product);
+    product.image = image
+    productsServices.updateProduct(id, product)
 
-    res.redirect("/products");
+    res.redirect('/products')
   },
   destroy: (req, res) => {
-    const id = req.params.id;
+    const id = req.params.id
 
-    productsServices.deleteProduct(id);
+    productsServices.deleteProduct(id)
 
-    res.redirect("/products");
+    res.redirect('/products')
   },
-};
+}
 
-module.exports = controller;
+module.exports = controller
