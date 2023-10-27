88eaeb3bf46c5401e09690cf122da26a317d7b74
Martiniano <bianchimartiniano@gmail.com>
2023-10-27 16:09:03 -0300

    crud success

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,43 +1,93 @@
+const bcryptjs = require('bcryptjs')
+
 const usersServices = require('../services/usersServices')
 
 const controller = {
-    crud: (req, res) => {
-        const users = usersServices.getAllUsers()
+  crud: (req, res) => {
+    const users = usersServices.getAllUsers()
 
-        res.render('users-crud', { users })
-    },
-    create: (req, res) => {
-        res.render('users-create')
-    },
-    store: (req, res) => {
-        const {firstName, lastName, email, password, accessType, profilePicture} = req.body
+    res.render('users-crud', { users })
+  },
+  create: (req, res) => {
+    res.render('user-create')
+  },
+  store: (req, res) => {
+    const { firstName, lastName, email, password, accessType, image } = req.body
 
-        const user = {
-            firstName,
-            lastName,
-            email,
-            password,
-            accessType,
-            profilePicture: req.file ? req.file.filename : profilePicture,
-        }
-
-        const userInDB = usersServices.getUserByField('email', req.body.email);
-
-        if (userInDB) {
-          return res.render('users-create', {
-            errors: {
-              email: {
-                msg: 'Este correo electrónico ya ha sido registrado',
-              },
-            },
-            oldData: req.body,
-          });
-        }
-
-        usersServices.createUser(user);
-
-        res.redirect('/users/crud');
+    const user = {
+      firstName,
+      lastName,
+      email,
+      password: bcryptjs.hashSync(password, 10),
+      accessType,
+      image: req.file ? req.file.filename : 'image-default.jpg',
     }
+
+    const userInDB = usersServices.getUserByField('email', req.body.email)
+
+    if (userInDB) {
+      return res.render('user-create', {
+        errors: {
+          email: {
+            msg: 'Este correo electrónico ya ha sido registrado',
+          },
+        },
+        oldData: req.body,
+      })
+    }
+
+    usersServices.createUser(user)
+
+    res.redirect('/users/crud')
+  },
+  detail: (req, res) => {
+    const id = req.params.id
+    const user = usersServices.getUser(id)
+
+    res.render('user-detail', { user })
+  },
+  edit: (req, res) => {
+    const id = req.params.id
+    const user = usersServices.getUser(id)
+
+    res.render('user-edit', { user })
+  },
+  update: (req, res) => {
+    const { id } = req.params
+    const { firstName, lastName, email, password } = req.body
+    const user = {
+      firstName,
+      lastName,
+      email,
+    }
+
+    if (!password.length == 0) {
+      user.password = bcryptjs.hashSync(password, 10)
+    }
+
+    user.image = req.file ? req.file.filename : usersServices.getUser(id).image
+
+    usersServices.updateUser(id, user)
+
+    res.redirect('/users/crud')
+  },
+  delete: (req, res) => {
+    const { id } = req.params
+    const user = usersServices.getUser(id)
+
+    res.render('user-delete', { user })
+  },
+  destroy: (req, res) => {
+    const { id } = req.params
+    const user = req.body
+
+    const image = req.file ? req.file.filename : usersServices.getUser(id).image
+    user.image = image
+
+    usersServices.deleteUser(id)
+
+    res.redirect('/users/crud')
+  },
 }
 
 module.exports = controller
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,28 +1,36 @@
-const express = require("express");
-const router = express.Router();
-const path = require("path");
+const express = require('express')
+const router = express.Router()
+const path = require('path')
 
-const multer = require("multer");
+const multer = require('multer')
 
 const storage = multer.diskStorage({
-  destination: path.join(__dirname, "../../public/images/users"),
+  destination: path.join(__dirname, '../../public/images/users'),
   filename: function (req, file, cb) {
     cb(
       null,
-      file.fieldname + "-" + Date.now() + path.extname(file.originalname),
-    );
+      file.fieldname + '-' + Date.now() + path.extname(file.originalname)
+    )
   },
-});
+})
 
 const upload = multer({
   storage: storage,
-});
+})
 
-const usersController = require("../controllers/usersController");
+const usersController = require('../controllers/usersController')
 
-router.get("/crud", usersController.crud);
+router.get('/crud', usersController.crud)
 
-router.get("/create", usersController.create);
-router.post("/", upload.single("profilePicture"), usersController.store);
+router.get('/create', usersController.create)
+router.post('/', upload.single('image'), usersController.store)
 
-module.exports = router;
+router.get('/:id', usersController.detail)
+
+router.get('/:id/edit', usersController.edit)
+router.put('/:id/edit', upload.single('image'), usersController.update)
+
+router.get('/:id/delete', usersController.delete)
+router.delete('/:id/delete', usersController.destroy)
+
+module.exports = router
